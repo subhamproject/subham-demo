f649650856baec34ced3c4b3fa1e860c43c13fa9
Subham Mandal <subhammandal01@gmail.com>
2020-05-18 18:06:45 +0530

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -5,5 +5,5 @@ docker run -p 6060:6060 --link db:postgres -d --name clair arminc/clair-local-sc
 sleep 1
 DOCKER_GATEWAY=$(docker network inspect bridge --format "{{range .IPAM.Config}}{{.Gateway}}{{end}}")
 wget -qO clair-scanner https://github.com/arminc/clair-scanner/releases/download/v8/clair-scanner_linux_amd64 && chmod +x clair-scanner
-./clair-scanner --ip="$DOCKER_GATEWAY" -t High -r scan-report.json subham-test-image
-[ $? -ne 0 ] && { echo 'image "\subham-test-image\" has some vulnerabilities,please fix and re-run it again' ; exit 1 ; } || :
+./clair-scanner --ip="$DOCKER_GATEWAY" -t Critical -r scan-report.json subham-test-image
+[ $? -ne 0 ] && { echo 'image "subham-test-image" has some vulnerabilities,please fix and re-run it again' ; exit 1 ; } || :
